b7640f4c8b752d9d406e58734f837b80816c5b97
Baran Kalakoğlu <barankalakoglu7@gmail.com>
2022-12-23 16:12:34 +0300

    Home Page added.

--- BulkyBookWeb/Areas/Customer/Controllers/HomeController.cs
+++ BulkyBookWeb/Areas/Customer/Controllers/HomeController.cs
@@ -1,4 +1,6 @@
-﻿using BulkyBook.Models;
+﻿using BulkyBook.DataAccess.Repository;
+using BulkyBook.DataAccess.Repository.IRepository;
+using BulkyBook.Models;
 using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
 
@@ -7,15 +9,29 @@ namespace BulkyBookWeb.Areas.Customer.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly IUnitofWork _unitofWork; //to access all the products.
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger,IUnitofWork unitofwork)
         {
             _logger = logger;
+            _unitofWork = unitofwork;
         }
 
         public IActionResult Index()
         {
-            return View();
+            IEnumerable<Product> productList = _unitofWork.Product.GetAll(includeProperties:"Category,CoverType"); // Product'ın içindeki bütün verilere sahip product listesi oluşturduk. Category ve CoverType'larınında gelmesini istediğimiz için onları includeladık.
+            return View(productList);
+        }
+
+        public IActionResult Details(int id)
+        {
+            ShoppingCart cartObj = new()
+            {
+                Count = 1,
+                Product = _unitofWork.Product.GetFİrstOrDefault(u => u.Id == id, includeProperties: "Category,CoverType"),
+            };
+
+            return View(cartObj);
         }
 
         public IActionResult Privacy()
